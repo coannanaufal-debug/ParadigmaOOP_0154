9835bce32c82aa11b29102dc8f9d289f7cfd651b
coannanaufal-debug <coanna.naufal@gmail.com>
2026-04-20 14:44:08 +0700

    membuat class barang dan void nya

--- ActivityOOP.cpp
+++ ActivityOOP.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 using namespace std;
 
+class Barang {
+public:
+    string nama;
+    int jumlah;
+    string kategori;
+    string tanggalproduksi;
+
+    void printData(){
+        cout << "Nama Barang       : " << nama << endl;
+        cout << "Jumlah Barang     : " << jumlah << endl;
+        cout << "Kategori Barang   : " << kategori << endl;
+        cout << "Tanggal Produksi  : " << tanggalproduksi << endl;
+    } 
+};
